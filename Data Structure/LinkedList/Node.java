98e13ebc21452d4f5d725fe35f5c16b4fea48885
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2020-09-15 22:46:40 +0700

    Create Node.java

---
+++ Data Structure/LinkedList/Node.java
@@ -0,0 +1,14 @@
+public class Node {
+    int value;
+    Node next;
+    public Node(){
+    }
+
+    public Node(int value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+    public Node(int value){
+        this.value = value;
+    }
+}
