6f8a88930b423a62461a1a1b76f443ccfa75e8e5
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2020-09-24 21:55:54 +0700

    Create HanoiTower.java

---
+++ Algorithms/HanoiTower.java
@@ -0,0 +1,13 @@
+public class HanoiTower{
+    void move(int n, char source, char destination, char auxiliary){  
+        if(n==1) System.out.println(source+"--->"+destination);
+        else{
+            move(n-1,source,auxiliary,destination);
+            move(1,source,destination,auxiliary);
+            move(n-1,auxiliary,destination,source);
+        }
+    }
+    public static void main(String[] args){
+         HanoiTower ht = new HanoiTower();
+         ht.move(3,'A','B','C');  //move n disk from rod A to rod B
+    
