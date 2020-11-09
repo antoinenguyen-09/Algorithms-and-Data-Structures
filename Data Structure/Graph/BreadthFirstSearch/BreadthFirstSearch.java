03cf95db3db7d0dabadbe50de750eafc74b17908
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2020-11-09 11:17:11 +0700

    Update BreadthFirstSearch.java

--- Data Structure/Graph/BreadthFirstSearch/BreadthFirstSearch.java
+++ Data Structure/Graph/BreadthFirstSearch/BreadthFirstSearch.java
@@ -53,11 +53,6 @@ public class BreadthFirstSearch {
         } catch(Exception e){
             System.out.println("Co loi khi doc file"+e.getMessage());
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<i; j++){
-                a[i][j] = a[j][i];
-            }
-        }
     }
     void display(){
         for(int i=0; i<n; i++){
